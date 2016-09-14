80ae3b59003b5e6934f087f14790d5540ddbdc56
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-09-14 11:28:43 +0100

    distance measure

---
+++ euclid.py
@@ -0,0 +1,11 @@
+import math;
+
+def dist(x,y):
+    n = len(x);
+    if n != len(y):
+        raise TypeError('Different number of arguments');
+    else:
+        d = 0;
+        for i in range(n):
+            d+=(x[i]-y[i])**2
+        return math.sqrt(d);
