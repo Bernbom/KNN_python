68881b7a1b19fb0bd516167f0b6e39fb04e6710f
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-09-14 20:44:46 +0100

    some beautification

--- knn.py
+++ knn.py
@@ -1,5 +1,5 @@
-import euclid as e;
-from math import floor;
+import euclid as e
+from math import floor
 
 def knn(learn_data, learn_labels, test_data, k):
     """
@@ -9,36 +9,28 @@ def knn(learn_data, learn_labels, test_data, k):
     with labels being 1 and -1
     """
     if k<1:
-        raise ValueError('k must be possitive, k = '+str(k));
-    elif k%2==0:
-        raise ValueError('k must be odd, k = '+str(k));
-    else:
-        test_labels = [];
-        learn_data = list(rev_enumerate(learn_data));
-        for point in test_data:
-            test_labels.append(find_label(learn_data, learn_labels, point, k));
-        return test_labels;
+        raise ValueError('k must be possitive, k = '+str(k))
+    if k%2==0:
+        raise ValueError('k must be odd, k = '+str(k))
+    test_labels = []
+    learn_data = list(enumerate(learn_data))
+    for point in test_data:
+        test_labels.append(find_label(learn_data, learn_labels, point, k))
+    return test_labels
     
 def find_label(learn_data, learn_labels, point, k):
     """
     find the label of a given point using k-nearest-neighbor
     """
-    distances = [];
+    distances = []
     for data in learn_data:
-        distances.append((e.dist(data[0],point),data[1]));
-    distances.sort();
+        distances.append((e.dist(data[1],point),data[0]))
+    distances.sort()
     
     if [learn_labels[i] for (d,i) in distances[:k]].count(1) > floor(k/2):
-        label = 1;
+        label = 1
     else:
-        label = -1;
+        label = -1
     return label
     
-def rev_enumerate(sequence, start=0):
-    """
-    enumerate the data with the index being second argument in the result
-    """
-    n = start
-    for elem in sequence:
-        yield elem, n
-        n += 1
+
